7e941c80942904a4850781a468fc8fab499deb15
swayamji <aroraswayam992@gmail.com>
2024-11-15 18:51:39 +0530

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-#Clone
-
-This is a website clone of anzo.studio, If you want to  take a look, you can see it at https://swayamji.github.io/Anzo-Clone-/
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Anzo-Clone-/'  // This should match the repo name
-})
